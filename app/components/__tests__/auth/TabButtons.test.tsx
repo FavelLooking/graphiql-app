35da44b1cef6ad65dc38b35101ea96f2ad48b7aa
Maxim <maximozaitsev@gmail.com>
2024-09-09 17:18:54 +0400

    feat: add test for TabButtonsComponent

---
+++ app/components/__tests__/auth/TabButtons.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import TabButtons from "../../auth/TabButtons";
+import { vi } from "vitest";
+import { I18nextProvider } from "react-i18next";
+import i18n from "i18next";
+
+i18n.init({
+  lng: "en",
+  resources: {
+    en: {
+      translation: {
+        "buttons.login": "Login",
+        "buttons.register": "Register",
+      },
+    },
+  },
+});
+
+describe("TabButtons", () => {
+  it("renders the login and register buttons", () => {
+    const mockOnTabChange = vi.fn();
+
+    render(
+      <BrowserRouter>
+        <I18nextProvider i18n={i18n}>
+          <TabButtons activeTab="login" onTabChange={mockOnTabChange} />
+        </I18nextProvider>
+      </BrowserRouter>
+    );
+
+    expect(screen.queryByText("Login")).not.toBeNull();
+    expect(screen.queryByText("Register")).not.toBeNull();
+  });
+});
