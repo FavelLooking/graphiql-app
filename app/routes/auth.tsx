2810c8da424951cd338b7e3dede0d665f2ac44d3
Maxim <maximozaitsev@gmail.com>
2024-09-11 16:30:38 +0400

    fix: notifi display on auth redirect

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
@@ -7,12 +7,16 @@ import AuthContainer from "../components/auth/AuthContainer";
 export default function AuthPage() {
   const navigate = useNavigate();
   const token = useSelector((state: RootState) => state.auth.token);
+  const [isRedirecting, setIsRedirecting] = useState(false);
 
   useEffect(() => {
-    if (token) {
-      navigate("/", { replace: true });
+    if (token && !isRedirecting) {
+      setIsRedirecting(true);
+      setTimeout(() => {
+        navigate("/", { replace: true });
+      }, 2000);
     }
-  }, [token, navigate]);
+  }, [token, isRedirecting, navigate]);
 
   return <AuthContainer />;
 }
