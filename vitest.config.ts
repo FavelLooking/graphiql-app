82b388aa4d0f771214ecf24491c8d6a0a99e9f1b
FavelLooking <intwestpol@gmail.com>
2024-09-13 18:19:49 +0400

    tests: exclude files from test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,15 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
+    coverage: {
+      exclude: [
+        "graphiql-app/app/entry.client.tsx",
+        "graphiql-app/app/entry.server.tsx",
+        "graphiql-app/app/root.tsx",
+        "postcss.config.js",
+        "tailwind.config.ts",
+        "vitest.setup.ts",
+      ],
+    },
   },
 });
