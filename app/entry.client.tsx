f32ee7fd221bd4d07ac59a6c2efb50ed6a20df21
FavelLooking <intwestpol@gmail.com>
2024-09-16 22:17:36 +0400

    fix: add new initialization point

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -3,7 +3,7 @@
  * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
  * For more information, see https://remix.run/file-conventions/entry.client
  */
-
+import "./utils/i18n";
 import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
