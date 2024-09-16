e416a30f254fed25dd47abf79edbf0b83f82c498
FavelLooking <intwestpol@gmail.com>
2024-09-16 22:05:29 +0400

    fix: initialize i18n

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -7,6 +7,7 @@
 import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
+import "./utils/i18n";
 
 startTransition(() => {
   hydrateRoot(
