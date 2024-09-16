b64dfed674b977f50c66606562be172a701c906f
FavelLooking <intwestpol@gmail.com>
2024-09-16 22:16:33 +0400

    fix: add new initialization point

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -7,7 +7,6 @@
 import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
-import "./utils/i18n";
 
 startTransition(() => {
   hydrateRoot(
